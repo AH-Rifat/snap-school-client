2520fdcfb605d42b083cb758baa652fd2d60a2ff
ah-rifat <rifat266@gmail.com>
2023-06-14 22:42:25 +0600

    student show enrolled and payment histoy - done

--- src/pages/dashboard/student/MyEnrolledClass.jsx
+++ src/pages/dashboard/student/MyEnrolledClass.jsx
@@ -1,39 +1,55 @@
 import { Card, CardContent, Container } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import useAuth from '../../../hooks/useAuth';
 
 
 const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
+    // { field: 'id', headerName: 'ID', width: 70 },
     { field: 'className', headerName: 'Class Name', width: 130 },
-    { field: 'instructorName', headerName: 'Instructor Name', width: 130 },
     {
         field: 'price',
         headerName: 'Price',
         type: 'number',
         width: 90,
     },
+    { field: 'status', headerName: 'Status', width: 180 },
 ];
 
-const rows = [
-    { id: 1, className: 'Snow', instructorName: 'Jon', price: 35 },
-    { id: 2, className: 'Lannister', instructorName: 'Cersei', price: 42 },
-    { id: 3, className: 'Lannister', instructorName: 'Jaime', price: 45 },
-    { id: 4, className: 'Stark', instructorName: 'Arya', price: 16 },
-    { id: 5, className: 'Targaryen', instructorName: 'Daenerys', price: null },
-    { id: 6, className: 'Melisandre', instructorName: null, price: 150 },
-    { id: 7, className: 'Clifford', instructorName: 'Ferrara', price: 44 },
-    { id: 8, className: 'Frances', instructorName: 'Rossini', price: 36 },
-    { id: 9, className: 'Roxie', instructorName: 'Harvey', price: 65 },
-];
+
 
 const MyEnrolledClass = () => {
+    const { user } = useAuth();
+    const [showEnrolledStudents, setshowEnrolledStudents] = useState([]);
+
+    useEffect(() => {
+        const fetchPaymentHistory = async () => {
+            try {
+                const response = await axios.get(
+                    `${import.meta.env.VITE_SERVER_URL}/payment/${user?.email}`
+                );
+                const rows = response.data.map((payment, index) => ({
+                    id: index + 1,
+                    ...payment,
+                }));
+                setshowEnrolledStudents(rows);
+            } catch (error) {
+                console.error('Error fetching payment history:', error);
+            }
+        };
+
+        fetchPaymentHistory();
+    }, [user]);
+
+
     return (
         <Container>
             <Card>
                 <CardContent>
                     <div style={{ height: 400, width: '100%' }}>
                         <DataGrid
-                            rows={rows}
+                            rows={showEnrolledStudents}
                             columns={columns}
                             initialState={{
                                 pagination: {
--- src/pages/dashboard/student/PaymentHistory.jsx
+++ src/pages/dashboard/student/PaymentHistory.jsx
@@ -1,10 +1,63 @@
+import { Card, CardContent, Container } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import useAuth from '../../../hooks/useAuth';
 
+const columns = [
+    { field: 'transactionId', headerName: 'Transaction ID', width: 300 },
+    { field: 'className', headerName: 'Class Name', width: 130 },
+    { field: 'price', headerName: 'Price', type: 'number', width: 90 },
+    {
+        field: 'date',
+        headerName: 'Date',
+        type: 'date',
+        width: 180,
+        valueFormatter: (params) => {
+            const date = new Date(params.value);
+            const options = { day: 'numeric', month: 'short', year: 'numeric', weekday: 'short' };
+            return date.toLocaleDateString(undefined, options);
+        },
+        align: 'center',
+    },
+    
+];
 
 const PaymentHistory = () => {
+    const { user } = useAuth();
+    const [paymentHistorys, setPaymentHistorys] = useState([]);
+
+    useEffect(() => {
+        const fetchPaymentHistory = async () => {
+            try {
+                const response = await axios.get(
+                    `${import.meta.env.VITE_SERVER_URL}/payment/${user?.email}`
+                );
+                const rows = response.data.map((payment, index) => ({
+                    id: index + 1,
+                    ...payment,
+                }));
+                setPaymentHistorys(rows);
+            } catch (error) {
+                console.error('Error fetching payment history:', error);
+            }
+        };
+
+        fetchPaymentHistory();
+    }, [user]);
+
+    console.log(paymentHistorys);
+
     return (
-        <div>
-            history
-        </div>
+        <Container>
+            <Card>
+                <CardContent>
+                    <div style={{ height: 400, width: '100%' }}>
+                        <DataGrid rows={paymentHistorys} columns={columns} pagination pageSize={5} />
+                    </div>
+                </CardContent>
+            </Card>
+        </Container>
     );
 };
 
